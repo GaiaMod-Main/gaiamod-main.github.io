f65af323f21bb6ec26c51f4fab1bb4a3e7900800
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-05-07 11:03:36 -0400

    Update GaiaBlocks.js

--- static/GaiaBlocks.js
+++ static/GaiaBlocks.js
@@ -25,7 +25,7 @@ class GaiaBlocks {
     return {
       id: 'gaiaBlocks',
       name: 'Gaia Blocks',
-	  color1: "#2BA6E1",
+	  color1: "#007BFF",
 menuIconURI: "data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHdpZHRoPSIyMTcuMjA4NDYiIGhlaWdodD0iMjA4LjI3MTEzIiB2aWV3Qm94PSIwLDAsMjE3LjIwODQ2LDIwOC4yNzExMyI+PGcgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoLTIxMS4zOTU3NywtNzMuNjQ1MDkpIj48ZyBmaWxsPSIjMDBhMWZmIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMTUiIHN0cm9rZS1taXRlcmxpbWl0PSIxMCI+PHBhdGggZD0iTTMxOS45OTk5OSw5Ni42Mzg5OGwyMS41MDkyLDYyLjMzNzUzbDY1LjQ5NzM0LDEuMzQ0NjJsLTUyLjIwMzkzLDM5Ljg3MTMzbDE4Ljk3MDM5LDYzLjE2ODU1bC01My43NzMsLTM3LjY5NTdsLTUzLjc3MywzNy42OTU3bDE4Ljk3MDM5LC02My4xNjg1NWwtNTIuMjAzOTMsLTM5Ljg3MTMzbDY1LjQ5NzM0LC0xLjM0NDYyeiIvPjwvZz48L2c+PC9zdmc+",
 blockIconURI: "data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHdpZHRoPSIyMTcuMjA4NDYiIGhlaWdodD0iMjA4LjI3MTEzIiB2aWV3Qm94PSIwLDAsMjE3LjIwODQ2LDIwOC4yNzExMyI+PGcgdHJhbnNmb3JtPSJ0cmFuc2xhdGUoLTIxMS4zOTU3NywtNzMuNjQ1MDkpIj48ZyBmaWxsPSIjMDBhMWZmIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMTUiIHN0cm9rZS1taXRlcmxpbWl0PSIxMCI+PHBhdGggZD0iTTMxOS45OTk5OSw5Ni42Mzg5OGwyMS41MDkyLDYyLjMzNzUzbDY1LjQ5NzM0LDEuMzQ0NjJsLTUyLjIwMzkzLDM5Ljg3MTMzbDE4Ljk3MDM5LDYzLjE2ODU1bC01My43NzMsLTM3LjY5NTdsLTUzLjc3MywzNy42OTU3bDE4Ljk3MDM5LC02My4xNjg1NWwtNTIuMjAzOTMsLTM5Ljg3MTMzbDY1LjQ5NzM0LC0xLjM0NDYyeiIvPjwvZz48L2c+PC9zdmc+",
       blocks: [
@@ -66,6 +66,11 @@ blockIconURI: "data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHR
             disableMonitor: true,
         },
           {
+            opcode: 'isPenguinMod',
+            blockType: Scratch.BlockType.BOOLEAN,
+            text: 'Is a PenguinMod fork?'
+          },
+          {
             opcode: 'loadExtension',
             blockType: Scratch.BlockType.COMMAND,
             text: 'load an extension from [TEXT]',
@@ -77,9 +82,9 @@ blockIconURI: "data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHR
             },
           },
           {
-            opcode: "restartProject",
+            opcode: 'restartProject',
             blockType: Scratch.BlockType.COMMAND,
-            text: 'restart a project',
+            text: 'Restart a project',
             arguments: {
               TEXT: {
                 type: Scratch.ArgumentType.STRING,
@@ -87,6 +92,45 @@ blockIconURI: "data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHR
               },
             },
           },
+          {
+            opcode: 'enableDebug',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Enable debug mode',
+            arguments: {
+              TEXT: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "0",
+              },
+            },
+          },
+         {
+        opcode: 'widescreen',
+        blockType: Scratch.BlockType.COMMAND,
+        text: 'Widescreen',
+        },
+         {
+        opcode: 'normal',
+        blockType: Scratch.BlockType.COMMAND,
+        text: 'Standard screen',
+        },
+         {
+        opcode: 'widehd',
+        blockType: Scratch.BlockType.COMMAND,
+        text: 'Widescreen HD',
+        },
+         {
+        opcode: 'normalhd',
+        blockType: Scratch.BlockType.COMMAND,
+        text: 'Standard HD',
+        },
+		{
+          opcode: 'sayName',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'Say [TEXT]',
+          arguments: {
+            TEXT: { type: Scratch.ArgumentType.STRING }
+          }
+        },
 		/////lols
       ],
     };
@@ -138,15 +182,54 @@ blockIconURI: "data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHR
    closeTabDisabled() {
             return CloseTabDisabled;
    }
+    isPenguinMod() {
+      this.isem = Scratch.extensions.isPenguinMod
+    ? "true"  : "false";
+  return this.isem
+  
+    }
     restartProject() {
       vm.greenFlag();
     }
-
+    enableDebug() {
+      vm.enableDebug();
+    }
     async loadExtension({ TEXT }) {
       if (await vm.securityManager.canLoadExtensionFromProject(TEXT)) {
         vm.extensionManager.loadExtensionURL(TEXT);
       }
     }
+widescreen() {
+        let width = 640;
+        let height = 360;
+        if (width <= 0) width = 1;
+        if (height <= 0) height = 1;
+        if (vm) vm.setStageSize(width, height);
+    }
+    normal() {
+        let width = 480;
+        let height = 360;
+        if (width <= 0) width = 1;
+        if (height <= 0) height = 1;
+        if (vm) vm.setStageSize(width, height);
+    }
+  widehd() {
+        let width = 1280;
+        let height = 720;
+        if (width <= 0) width = 1;
+        if (height <= 0) height = 1;
+        if (vm) vm.setStageSize(width, height);
+    }
+  normalhd() {
+        let width = 960;
+        let height = 720;
+        if (width <= 0) width = 1;
+        if (height <= 0) height = 1;
+        if (vm) vm.setStageSize(width, height);
+    }
+	  sayName({ TEXT }) {
+    return TEXT;
+  }
 }
     Scratch.extensions.register(new GaiaBlocks(Scratch.vm.runtime));
 })(Scratch);
