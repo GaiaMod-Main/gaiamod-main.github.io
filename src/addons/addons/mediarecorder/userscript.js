4f73718af4830ee697f8c0867058111b4806b621
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-05-08 15:33:23 -0400

    Update userscript.js

--- src/addons/addons/mediarecorder/userscript.js
+++ src/addons/addons/mediarecorder/userscript.js
@@ -378,18 +378,18 @@ export default async ({ addon, console, msg }) => {
             }, (delay - roundedDelay) * 1000);
         };
         if (!recordElem) {
-            recordElem = Object.assign(document.createElement("div"), {
-                className: "sa-record " + elem.className,
-            });
-            const icon = Object.assign(document.createElement("img"), {
-             className: "sa-record " + elem.className,
-             src: recordIcon,
-             });
-       recordElem.appendChild(icon);
+                  recordElem = Object.assign(document.createElement("div"), {
+        className: "sa-record " + elem.className,
+      });
+      const icon = Object.assign(document.createElement("img"), {
+        src: recordIcon,
+        className: "sa-record-icon",
+      });
+      recordElem.appendChild(icon);
       recordTextSpan = Object.assign(document.createElement("span"), {
         textContent: msg("record"),
-        });
-           recordElem.appendChild(recordTextSpan);
+      });
+      recordElem.appendChild(recordTextSpan);
             recordElem.addEventListener("click", async () => {
                 if (isRecording) {
                     stopRecording();
