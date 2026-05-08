2224f0a586c9d12edd431b5e195e2ecaa25c238f
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-05-08 15:59:54 -0400

    Update userscript.js

--- src/addons/addons/mediarecorder/userscript.js
+++ src/addons/addons/mediarecorder/userscript.js
@@ -380,7 +380,7 @@ export default async ({ addon, console, msg }) => {
         if (!recordElem) {
             recordElem = Object.assign(document.createElement("div"), {
                 className: "sa-record " + elem.className,
-                textContent: msg("Record"),
+                textContent: msg("record"),
             });
             recordElem.addEventListener("click", async () => {
                 if (isRecording) {
