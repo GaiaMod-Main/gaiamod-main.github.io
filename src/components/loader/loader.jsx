918bd8b7fea52b228893255353a75bb18ed15e69
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-05-15 06:11:28 -0400

    Update loader.jsx

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -152,7 +152,7 @@ class LoaderComponent extends React.Component {
                 <div className={styles.container}>
 				    <img width="250px" src={logo} alt="logo"/>
                     <div className={styles.blockAnimation}>
-                        <img width="150px" src={stars} alt="bouncer"/>
+                        <img src={stars} alt="bouncer"/>
                     </div>
                     <div className={styles.title}>
                         {mainMessages[this.props.messageId]}
