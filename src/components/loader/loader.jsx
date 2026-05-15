1c15fd53bb9d5a6c06cbee6118ff25a3caf94196
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-05-15 10:45:54 -0400

    Update loader.jsx

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -150,13 +150,12 @@ class LoaderComponent extends React.Component {
                 })}
             >
                 <div className={styles.container}>
-				    <img src={logo} alt="logo"/>
+                    <div className={styles.blockAnimation}>
+                        <img width="300" src={logo} alt="logo"/>
+                    </div>
 					<div style={{
                         textAlign: "right"
                     }}> //
-                    <div className={styles.blockAnimation}>
-                        <img src={stars} alt="bouncer"/>
-                    </div>
                     <div className={styles.title}>
                         {mainMessages[this.props.messageId]}
                     </div>
@@ -176,8 +175,8 @@ class LoaderComponent extends React.Component {
                         </div>
                     )}
                 </div>
-            </div>
-          </div> //
+            </div> //
+          </div> 
         );
     }    
 }
