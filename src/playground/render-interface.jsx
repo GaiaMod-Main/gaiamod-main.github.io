0c55615aa522febebc7acca5bab441f3b3c3c9d1
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-05-22 23:01:25 -0400

    Update render-interface.jsx

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -599,7 +599,7 @@ class Interface extends React.Component {
                             </div>
                             <a
                                 target="_blank"
-                                href="https://penguinmod.com/search?q=newest:"
+                                href="https://penguinmod.com/search?q=featured:"
                                 rel="noreferrer"
                             >
                                 See more projects
