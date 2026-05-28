03be322e2a3474de8ca91a2f57e726cbb06e98d9
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-05-28 09:31:42 -0400

    ha!

---
+++ install and run.sh
@@ -0,0 +1,9 @@
+echo "Installing dependencies using npm i..."
+npm i --force
+
+echo "Running npm run build..."
+npm run build --force
+
+echo "The script has fucking finished lol!"
+
+# Cat CIRCLING CHASED BY ANGRY ROBOTS lol
