d8d2a08c6d6867ae465fba46e774ab13ddbeec25
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-06-01 17:28:17 -0400

    a

--- src/containers/tw-security-manager.jsx
+++ src/containers/tw-security-manager.jsx
@@ -76,6 +76,7 @@ const isTrustedExtensionOrigin = url => (
     url.startsWith('https://banana-mod.github.io/') ||
     url.startsWith('https://omniblocks.github.io/') ||
     url.startsWith('https://m.ccw.site/user_projects_assets/') || //Cocrea gets it!
+    url.startsWith('https://khanning.github.io/scratch-arduino-extension/') ||
     url.startsWith('https://bludisanlemon.github.io/lemons-gallery/extensions/Lemon/') ||
     url.startsWith('https://raw.githubusercontent.com/David-Orangemoon/Modified-Extension-Loader-Turbowarp/main/custom%20extensions') ||
     url.startsWith('https://raw.githubusercontent.com/ningqi24/TurbowarpExtension/refs/heads/main/.js/') ||
