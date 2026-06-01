a82097041157945fb0bd63537ac4e80d273b7caf
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-06-01 01:25:59 -0400

    HI

--- src/components/backpack/backpack.jsx
+++ src/components/backpack/backpack.jsx
@@ -52,7 +52,6 @@ const Backpack = ({
     showMore,
     onToggle,
     onDelete,
-	onExport,
     onRename,
     onMouseEnter,
     onMouseLeave,
@@ -125,7 +124,6 @@ const Backpack = ({
                                         name={intl.formatMessage(labelMap[item.type])}
                                         selected={false}
                                         onClick={noop}
-										onExportButtonClick={item.type === 'script' ? null : onExport}
                                         onDeleteButtonClick={onDelete}
                                         // Currently, renaming sprites is not supported.
                                         onRenameButtonClick={item.type === 'sprite' ? null : onRename}
@@ -175,7 +173,6 @@ Backpack.propTypes = {
     intl: intlShape,
     loading: PropTypes.bool,
     onDelete: PropTypes.func,
-	onExport: PropTypes.func,
     onRename: PropTypes.func,
     onMore: PropTypes.func,
     onMouseEnter: PropTypes.func,
