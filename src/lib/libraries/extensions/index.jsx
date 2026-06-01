81d1f25d2d3c0510b77f072e4b411b540cecf34c
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-06-01 16:27:56 -0400

    Update index.jsx

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -303,8 +303,8 @@ import axerAIIconURL from './other/AxerAI.svg';
 import axerAIInsetIconURL from './other/InsetAxerAI.png';
 import nftIconURL from './nft/nft.png';
 import nftInsetIconURL from './nft/nft-small.svg';
-import toonco1ImageURL from "./webkit/webKit.png";
-import toonco1ImageSmallURL from "./webkit/Webkit-small.png";
+import toonco1ImageURL from "./webKit/webKit.png";
+import toonco1ImageSmallURL from "./webKit/Webkit-small.png";
 
 // ESP32
 import esp32SerialIconURL from './zumiAI/zumiAI.png';
