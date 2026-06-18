3f859d1377bdfb103fa76ed213088a3a94b24750
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-06-18 15:23:16 -0400

    Uploaded the source code.

--- upload to github.bat
+++ upload to github.bat
@@ -4,5 +4,5 @@ git add .
 git commit -m "Uploaded the source code."
 git branch -M develop
 git remote add origin https://github.com/GaiaMod-Main/gaiamod-main.github.io.git
-git push -f --no-verify origin main
+git push -f --no-verify origin develop
 PAUSE
