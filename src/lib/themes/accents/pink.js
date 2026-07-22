5ddfb9aed760ea6ea20e682b089a6ad31ccae920
~ Gaia ~ <crystalfranks18@hotmail.com>
2026-07-22 03:24:03 +0000

    PINKY

--- src/lib/themes/accents/pink.js
+++ src/lib/themes/accents/pink.js
@@ -7,6 +7,13 @@ const guiColors = {
     'looks-transparent': 'hsla(325, 60%, 60%, 0.35)',
     'looks-light-transparent': 'hsla(325, 60%, 60%, 0.15)',
     'looks-secondary-dark': 'hsla(325, 42%, 51%, 1)'
+
+    'extensions-primary': 'rgb(255, 117, 232)',
+    'extensions-tertiary': 'rgb(206, 47, 158)',
+    'extensions-transparent': 'hsla(318, 76%, 40%, 0.35)',
+    'extensions-light': 'rgb(236, 31, 192)',
+    
+    'drop-highlight': 'rgb(233, 29, 240)'
 };
 
 const blockColors = {};
